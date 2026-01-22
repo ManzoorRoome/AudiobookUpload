11e834284c5bedc3036c291d3a3b307022ef122b
Manzoor <roome.manzoor@gmail.com>
2026-01-23 02:11:09 +0300

    Update files

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func main() {
 	link := os.Args[1]
 	var name string
-	fmt.Print("Filename (without extension): ")
-	fmt.Scanln(&name)
+	fmt.Print("Filename: ")
+	fmt.Scanf("%s", &name)
 	exec.Command("yt-dlp", "-f", "bestaudio", "--embed-thumbnail", "--add-metadata", "--audio-format", "mp3", "-o", "C:/Users/Manzo/Downloads/"+name+".%(ext)s", link).Run()
 	exec.Command("adb", "shell", "mkdir", "-p", "/sdcard/audiobooks/"+name).Run()
-	exec.Command("adb", "push", "C:/Users/Manzo/Downloads/"+name+".mp3", "/sdcard/audiobooks/"+name+"/").Run()
-	os.Remove("C:/Users/Manzo/Downloads/" + name + ".mp3")
+	files, _ := filepath.Glob("C:/Users/Manzo/Downloads/" + name + ".*")
+	for _, file := range files {
+		exec.Command("adb", "push", file, "/sdcard/audiobooks/"+name+"/").Run()
+		os.Remove(file)
+	}
 }
